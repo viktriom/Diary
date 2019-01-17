48550ccc6db8824d50f18335496f71653c00c148
Vivek Tripathi <viktriom@gmail.com>
2019-01-17 07:32:16 +0530

    Added encryption Functionality.

--- app/src/test/java/com/sonu/diary/util/SecurityUtilTest.java
+++ app/src/test/java/com/sonu/diary/util/SecurityUtilTest.java
@@ -3,50 +3,20 @@ package com.sonu.diary.util;
 import org.bouncycastle.cms.CMSException;
 import org.junit.Test;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.AlgorithmParameters;
 import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Security;
 import java.security.cert.CertificateEncodingException;
-import java.security.spec.X509EncodedKeySpec;
-import java.sql.SQLException;
-import java.util.Date;
-
 import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyAgreement;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.ShortBufferException;
-import javax.crypto.interfaces.DHPublicKey;
-import javax.crypto.spec.DHParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
+
 
 public class SecurityUtilTest {
 
-    @Test
-    public void testSecurityUtilOld() throws CertificateEncodingException, IOException, CMSException {
-        SecurityUtil.init(new FileInputStream("/Users/sonu/dev/proj/android/Diary/app/src/test/java/com/sonu/diary/util/Baeldung.cer"), new FileInputStream("/Users/sonu/dev/proj/android/Diary/app/src/test/java/com/sonu/diary/util/Baeldung.p12"));
-        String secretMessage = "My password is 123456Seven";
-        System.out.println("Original Message : " + secretMessage);
-        byte[] stringToEncrypt = secretMessage.getBytes();
-        byte[] encryptedData = SecurityUtil.encryptDataBC(stringToEncrypt, SecurityUtil.certificate);
-        System.out.println("Encrypted Message : " + new String(encryptedData));
-        byte[] rawData = SecurityUtil.decryptDataBC(encryptedData, SecurityUtil.privateKey);
-        String decryptedMessage = new String(rawData);
-        System.out.println("Decrypted Message : " + decryptedMessage);
-    }
 
     @Test
     public void testSecurityUtil() throws CertificateEncodingException, IOException, CMSException, InvalidKeyException {
