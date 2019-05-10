105a5c4d11bb3729a348306acb31e940b3326340
Vivek Tripathi <viktriom@gmail.com>
2019-05-10 08:04:31 +0530

    Modified the spinners used for Entry Title, Entry Event and Payment mode to be Autocomplete fields with options displayed always.

--- app/src/main/java/com/sonu/diary/activities/GeneralEntry.java
+++ app/src/main/java/com/sonu/diary/activities/GeneralEntry.java
@@ -1,27 +1,26 @@
 package com.sonu.diary.activities;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.ContentFrameLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Cache;
 import com.google.gson.Gson;
 import com.sonu.diary.R;
 import com.sonu.diary.caches.CacheManager;
+import com.sonu.diary.database.DatabaseOperations;
+import com.sonu.diary.database.DatabaseOperationsImpl;
 import com.sonu.diary.domain.DiaryEntry;
 import com.sonu.diary.domain.DiaryPage;
+import com.sonu.diary.domain.EntryEvent;
 import com.sonu.diary.domain.EntryTitle;
 import com.sonu.diary.domain.PaymentMode;
 import com.sonu.diary.domain.enums.ListType;
@@ -31,23 +30,20 @@ import com.sonu.diary.util.DateUtils;
 
 import java.sql.Timestamp;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class GeneralEntry extends AbstractActivity {
 
     private DiaryEntry de;
-    private Spinner ddEntryTitle;
-    private Spinner ddEntryEvent;
+    private AutoCompleteTextView acEntryTitle;
+    private AutoCompleteTextView acEntryEvent;
     private EditText entryLocation;
     private EditText entryDescription;
     private TextView creationTimeLabel;
     private TextView lastModifiedTimeLabel;
     private TextView txtEntryActionTime;
     private TextView txtExpenditureAmount;
-    private Spinner ddExpenditureSource;
+    private AutoCompleteTextView acExpenditureSource;
     private Switch swhIsSharable;
     private Boolean editMode = false;
 
@@ -55,7 +51,9 @@ public class GeneralEntry extends AbstractActivity {
     private ArrayAdapter<String> entryTitleAdapter;
     private ArrayAdapter<String> entryEventAdapter;
 
-    Map<String, String> titleMap = new HashMap<>();
+    private Map<String, String> titleMap = new HashMap<>();
+
+    private DatabaseOperations dbOper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,87 +70,67 @@ public class GeneralEntry extends AbstractActivity {
         }
     }
 
-    protected void onPostResume() {
-        super.onPostResume();
-        prepareDropDownList();
-    }
-
     public void prepareDropDownList(){
-        List<String> titleLst = new LinkedList<>();
-        titleLst.add("---Select Category---");
-        titleLst.add("Add New");
-
-        List<String> paymentModeLst = new LinkedList<>();
-        paymentModeLst.add("---Select Payment Mode---");
-        paymentModeLst.add("Add New");
-
-        List<String> eventLst = new LinkedList<>();
-        eventLst.add("---Select Event Name---");
-        eventLst.add("Add New");
-
-        if(null == entryTitleAdapter) {
-            entryTitleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new LinkedList<>());
-            entryTitleAdapter.addAll(titleLst);
-            entryTitleAdapter.addAll(CacheManager.getEntryTitleCache().getEntryTitleNames());
-        } else {
-            entryTitleAdapter.clear();
-            entryTitleAdapter.addAll(titleLst);
-            entryTitleAdapter.addAll(CacheManager.getEntryTitleCache().getEntryTitleNames());
-        }
-
-        if(null == entryEventAdapter) {
-            entryEventAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new LinkedList<>());
-            entryEventAdapter.addAll(eventLst);
-            entryEventAdapter.addAll(CacheManager.getEntryEventCache().getEventNames());
-        } else {
-            entryEventAdapter.clear();
-            entryEventAdapter.addAll(eventLst);
-            entryEventAdapter.addAll(CacheManager.getEntryEventCache().getEventNames());
-        }
-
-        if(null == paymentModeAdapter) {
-            paymentModeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new LinkedList<>());
-            paymentModeAdapter.addAll(paymentModeLst);
-            paymentModeAdapter.addAll(CacheManager.getPaymentModeCache().getPaymentModeDescriptions());
-        } else {
-            paymentModeAdapter.clear();
-            paymentModeAdapter.addAll(paymentModeLst);
-            paymentModeAdapter.addAll(CacheManager.getPaymentModeCache().getPaymentModeDescriptions());
-        }
-        ddEntryTitle.setAdapter(entryTitleAdapter);
-        ddExpenditureSource.setAdapter(paymentModeAdapter);
-        ddEntryEvent.setAdapter(entryEventAdapter);
+        entryTitleAdapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_item, CacheManager.getEntryTitleCache().getEntryTitleNames());
+        entryEventAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, CacheManager.getEntryEventCache().getEventNames());
+        paymentModeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, CacheManager.getPaymentModeCache().getPaymentModeDescriptions());
+        acEntryTitle.setAdapter(entryTitleAdapter);
+        acExpenditureSource.setAdapter(paymentModeAdapter);
+        acEntryEvent.setAdapter(entryEventAdapter);
     }
 
     private void init(){
         Log.i(GeneralEntry.class.getName(), "Initializing UI Objects.");
 
-        ddEntryTitle = findViewById(R.id.ddTitle);
-        ddEntryEvent = findViewById(R.id.ddEntryEvent);
+        dbOper = new DatabaseOperationsImpl(this);
+
+        acEntryTitle = findViewById(R.id.acTitle);
+        acEntryEvent = findViewById(R.id.acEntryEvent);
         entryLocation = findViewById(R.id.txtEntryLocation);
         entryDescription = findViewById(R.id.txtGeneralEntryDetail);
         creationTimeLabel = findViewById(R.id.lblCreationTime);
         lastModifiedTimeLabel = findViewById(R.id.lblLastModifiedOn);
         txtEntryActionTime = findViewById(R.id.txtEntryActionTime);
         txtExpenditureAmount = findViewById(R.id.txtExpenditure);
-        ddExpenditureSource = findViewById(R.id.ddExpenditureSource);
+        acExpenditureSource = findViewById(R.id.acExpenditureSource);
         swhIsSharable = findViewById(R.id.swhIsSharable);
 
         prepareDropDownList();
 
-        Context context = this;
-        ddEntryTitle.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+        acEntryTitle.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    acEntryTitle.showDropDown();
+
+            }
+        });
+
+
+        acExpenditureSource.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    acExpenditureSource.showDropDown();
+
+            }
+        });
+
+        acEntryEvent.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    acEntryEvent.showDropDown();
+
+            }
+        });
+
+        acEntryTitle.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String)parent.getItemAtPosition(position);
-                if("Add New".equals(selected)){
-                    Intent intent = new Intent(context, ListItemInput.class);
-                    intent.putExtra("listType", ListType.ENTRY_TITLE.name());
-                    intent.putExtra("message", "Enter the new Title to be added:");
-                    startActivity(intent);
-                } else if(position > 1){
-                    titleMap.put("category", selected);
-                }
+
             }
 
             @Override
@@ -161,18 +139,9 @@ public class GeneralEntry extends AbstractActivity {
             }
         });
 
-        ddExpenditureSource.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        acExpenditureSource.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String)parent.getItemAtPosition(position);
-                if("Add New".equals(selected)){
-                    Intent intent = new Intent(context, ListItemInput.class);
-                    intent.putExtra("listType", ListType.PAYMENT_MODES.name());
-                    intent.putExtra("message", "Enter the new Payment Mode to be added:");
-                    startActivity(intent);
-                } else if(position > 1) {
-                    de.setEntryExpenditureSource(selected);
-                }
             }
 
             @Override
@@ -181,18 +150,9 @@ public class GeneralEntry extends AbstractActivity {
             }
         });
 
-        ddEntryEvent.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        acEntryEvent.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String)parent.getItemAtPosition(position);
-                if("Add New".equals(selected)){
-                    Intent intent = new Intent(context, ListItemInput.class);
-                    intent.putExtra("listType", ListType.ENTRY_EVENT.name());
-                    intent.putExtra("message", "Enter the new Event to be added:");
-                    startActivity(intent);
-                } else if(position > 1) {
-                    titleMap.put("eventName", selected);
-                }
             }
 
             @Override
@@ -226,16 +186,16 @@ public class GeneralEntry extends AbstractActivity {
         if(de.getEntryTitle().contains("eventName")){
             Gson gson = new Gson();
             Map<String,String> map = gson.<Map<String, String>>fromJson(de.getEntryTitle(), Map.class);
-            ddEntryTitle.setSelection(entryTitleAdapter.getPosition(map.get("category")), false);
-            ddEntryEvent.setSelection(entryEventAdapter.getPosition(map.get("eventName")), false);
+            acEntryTitle.setText(map.get("category") == null ? "": map.get("category"));
+            acEntryEvent.setText(map.get("eventName") == null ? "" : map.get("eventName"));
         } else {
-            ddEntryTitle.setSelection(entryTitleAdapter.getPosition(de.getEntryTitle()), false);
+            acEntryTitle.setSelection(entryTitleAdapter.getPosition(de.getEntryTitle()));
         }
         entryLocation.setText(de.getLocation());
         entryDescription.setText(de.getEntryDescription());
         if (null != de.getEntryExpenditure()){
             txtExpenditureAmount.setText(de.getEntryExpenditure().toString());
-            ddExpenditureSource.setSelection(paymentModeAdapter.getPosition(de.getEntryExpenditureSource()), false);
+            acExpenditureSource.setText(de.getEntryExpenditureSource());
         }
         txtEntryActionTime.setText(DateUtils.getStringDateFromTimestampInFormat(de.getEntryActionTime(), DateUtils.DEFAULT_TIMESTAMP_FORMAT));
         creationTimeLabel.setText(DateUtils.getStringDateFromTimestampInFormat(de.getEntryCreatedOn(), DateUtils.DEFAULT_TIMESTAMP_FORMAT));
@@ -243,28 +203,34 @@ public class GeneralEntry extends AbstractActivity {
         lastModifiedTimeLabel.setText(String.format("%s%s", text, DateUtils.getStringDateFromTimestampInFormat(de.getEntryLastUpdatedOn(), DateUtils.DEFAULT_TIMESTAMP_FORMAT)));
         de.setEntryLastUpdatedOn(DateUtils.getCurrentTimestamp());
         swhIsSharable.setChecked(de.isSharable());
-        ddEntryTitle.setSelection(1, false);
     }
 
     public void saveGeneralEntry(View view){
         Log.i(GeneralEntry.class.getName(), "Populating the DiaryEntry bean.");
 
+        String title = acEntryTitle.getText().toString();
+        String event = acEntryEvent.getText().toString();
+        titleMap.put("eventName", title);
+        titleMap.put("category", event);
+        addValueToList(ListType.ENTRY_TITLE, title);
+        addValueToList(ListType.ENTRY_EVENT, event);
         DiaryPage currentPage = CacheManager.getDiaryCache().getDiaryPageForDate(de.getEntryActionTime());
         de.setDiaryPage(currentPage);
         de.setEntryDescription(entryDescription.getText().toString());
-        de.setEntryTitle((String) ddEntryTitle.getSelectedItem());
         de.setLocation(entryLocation.getText().toString());
         String expAmount = null == txtExpenditureAmount.getText()?null:txtExpenditureAmount.getText().toString();
         if(null != expAmount && !expAmount.isEmpty() && !expAmount.equals("0") && AppUtil.isInteger(expAmount, 10)){
             de.setExpenseAdded(true);
             de.setEntryExpenditure(Integer.parseInt(txtExpenditureAmount.getText().toString()));
-            de.setEntryExpenditureSource((String) ddExpenditureSource.getSelectedItem());
+            String expSource = acExpenditureSource.getText().toString();
+            de.setEntryExpenditureSource(expSource);
+            addValueToList(ListType.PAYMENT_MODES, expSource);
         } else {
             de.setExpenseAdded(false);
         }
 
         Gson gson = new Gson();
-        String title = gson.toJson(titleMap);
+        title = gson.toJson(titleMap);
         de.setEntryTitle(title);
         de.setSyncStatus(SyncStatus.P.name());
         if(validationPassed()) {
@@ -327,5 +293,29 @@ public class GeneralEntry extends AbstractActivity {
     public void isSharableTouched(View view) {
         Switch swh = (Switch)view;
         de.setSharable(swh.isChecked());
+    }
+
+    public void addValueToList(ListType listType, String value) {
+        if(null == value || value.isEmpty())
+            return;
+        if(ListType.ENTRY_TITLE == listType && entryTitleAdapter.getPosition(value) < 0 ) {
+            EntryTitle title = new EntryTitle();
+            title.setTitle(value);
+            dbOper.create(listType.getTypeClass(), title);
+            CacheManager.getEntryTitleCache().loadCache();
+            acEntryTitle.setAdapter(entryTitleAdapter);
+        } else if( ListType.PAYMENT_MODES == listType && paymentModeAdapter.getPosition(value) < 0){
+            PaymentMode paymentMode = new PaymentMode();
+            paymentMode.setDescription(value);
+            dbOper.create(listType.getTypeClass(), paymentMode);
+            CacheManager.getPaymentModeCache().loadCache();
+            acExpenditureSource.setAdapter(paymentModeAdapter);
+        } else if( ListType.ENTRY_EVENT == listType && entryTitleAdapter.getPosition(value) < 0){
+            EntryEvent entryEvent = new EntryEvent();
+            entryEvent.setEventName(value);
+            dbOper.create(listType.getTypeClass(), entryEvent);
+            CacheManager.getEntryEventCache().loadCache();
+            acEntryEvent.setAdapter(entryEventAdapter);
+        }
     }
 }
---
+++ app/src/main/java/com/sonu/diary/activities/Preferences.java
@@ -0,0 +1,18 @@
+package com.sonu.diary.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sonu.diary.R;
+
+public class Preferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+
+
+
+}
